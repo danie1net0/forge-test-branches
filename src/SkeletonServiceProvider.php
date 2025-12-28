2bf6dcd797eda3e57709d6b2ccbc5461786abc41
Daniel Neto <danielneto314@gmail.com>
2025-12-28 17:57:41 -0300

    Initial commit

---
+++ src/SkeletonServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace VendorName\Skeleton;
+
+use Spatie\LaravelPackageTools\{Package, PackageServiceProvider};
+use VendorName\Skeleton\Commands\SkeletonCommand;
+
+class SkeletonServiceProvider extends PackageServiceProvider
+{
+    public function configurePackage(Package $package): void
+    {
+        /*
+         * This class is a Package Service Provider
+         *
+         * More info: https://github.com/spatie/laravel-package-tools
+         */
+        $package
+            ->name('skeleton')
+            ->hasConfigFile()
+            ->hasViews()
+            ->hasMigration('create_migration_table_name_table')
+            ->hasCommand(SkeletonCommand::class);
+    }
+}
